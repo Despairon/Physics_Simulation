8b271b1166767af38fd6a8c921ece590a3cfc202
Rybalsky <MRybalsky@kiev.luxoft.com>
2017-05-22 12:43:49 +0300

    added Render class; made some corrections

---
+++ Physics_Simulation/Camera.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+using Tao.DevIl;
+
+namespace Physics_Simulation
+{
+    public class Camera
+    {
+        #region private_members
+
+        private Point3D eye_position;
+        private Point3D center_position;
+        private Point3D up_position;
+
+        #endregion
+
+        #region public_members
+
+        public Camera()
+        {
+            eye_position    = new Point3D(0,0,1);
+            center_position = new Point3D(0,0,0);
+            up_position     = new Point3D(0,1,0);
+        }
+
+        public void renderCamera()
+        {
+            // TODO: implement camera rotate
+            Glu.gluLookAt(eye_position.x,    eye_position.y,    eye_position.z,
+                          center_position.x, center_position.y, center_position.z,
+                          up_position.x,     up_position.y,     up_position.z);
+        }
+
+        #endregion
+    }
+}
--- Physics_Simulation/Render.cs
+++ Physics_Simulation/Render.cs
@@ -8,21 +8,34 @@ using System.Windows.Forms;
 
 namespace Physics_Simulation
 {
+    public struct Point3D
+    {
+        #region public members
+
+        public Point3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double x;
+        public double y;
+        public double z;
+
+        #endregion
+    }
+
     public static class Render
     {
         #region private_members
-        private static SimpleOpenGlControl graphics;
-        private static Timer               drawingTimer;
 
-        private static void renderCamera()
-        {
-            // TODO: implement camera rotate
-            Glu.gluLookAt(0.0, 0.0, 1.0,
-                          0.0, 0.0, 0.0,
-                          0.0, 1.0, 0.0);
-        }
+        private static SimpleOpenGlControl graphics      = null;
+        private static Timer               drawingTimer  = new Timer();
+        private static bool                initialized   = false;
+        private static Camera              camera        = new Camera();
 
-        private static int i = 0;
+        private static int i = 0; // FIXME: delete
 
         private static void drawAll(object sender, EventArgs e)
         {
@@ -30,36 +43,51 @@ namespace Physics_Simulation
 
             Gl.glLoadIdentity();
 
-            renderCamera();
+            /* TEST */
+            camera.renderCamera();
 
             Gl.glColor3d(0.1f, 0.7f, 0.1f);
 
             Gl.glPushMatrix();
 
             Gl.glTranslated(0, 0, -2);
-            Gl.glRotated(-30, 1, 0, 0);
             Gl.glRotated(i++, 0, 1, 0);
             Glut.glutWireSphere(1, 50, 50);
 
             Gl.glPopMatrix();
 
+            /* END TEST*/
+
+            //foreach (var obj in physic_objects)
+            //    obj.calculate_physics();
+
+            //foreach (var obj in physic_objects)
+            //    obj.draw();
+
+
+
             Gl.glFlush();
 
             graphics.Invalidate();
         }
+
         #endregion
 
 
         #region public_members
+
         public static UserConfiguration userConfiguration = new UserConfiguration();
 
         public struct UserConfiguration
         {
             #region private_members
+
             private int _FPS;
+ 
             #endregion
 
             #region public_members
+
             public void setDefaultConfiguration()
             {
                 _FPS = 60;
@@ -78,32 +106,38 @@ namespace Physics_Simulation
                     _FPS = value;
                 }
             }
+
             #endregion
         }
 
         public static void init(ref SimpleOpenGlControl canvas)
         {
-            graphics = canvas;
-            graphics.InitializeContexts();
-            Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
-            Gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
-            Gl.glViewport(0, 0, graphics.Width, graphics.Height);
-            Gl.glMatrixMode(Gl.GL_PROJECTION);
-            Gl.glLoadIdentity();
-            Glu.gluPerspective(45, (float)graphics.Width / (float)graphics.Height, 0.1, 200);
-            Gl.glMatrixMode(Gl.GL_MODELVIEW);
-            Gl.glLoadIdentity();
-            Gl.glEnable(Gl.GL_DEPTH_TEST);
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            if (!initialized)
+            {
+                graphics = canvas;
+                graphics.InitializeContexts();
+                Glut.glutInit();
+                Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+                Gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
+                Gl.glViewport(0, 0, graphics.Width, graphics.Height);
+                Gl.glMatrixMode(Gl.GL_PROJECTION);
+                Gl.glLoadIdentity();
+                Glu.gluPerspective(45, (float)graphics.Width / (float)graphics.Height, 0.1, 200);
+                Gl.glMatrixMode(Gl.GL_MODELVIEW);
+                Gl.glLoadIdentity();
+                Gl.glEnable(Gl.GL_DEPTH_TEST);
+                Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
 
-            userConfiguration.setDefaultConfiguration();
+                userConfiguration.setDefaultConfiguration();
 
-            drawingTimer = new Timer();
-            drawingTimer.Interval = 1000 / userConfiguration.FPS;
-            drawingTimer.Tick += new EventHandler(drawAll);
-            drawingTimer.Start();
+                drawingTimer.Interval = 1000 / userConfiguration.FPS;
+                drawingTimer.Tick += new EventHandler(drawAll);
+                drawingTimer.Start();
+
+                initialized = true;
+            }
         }
+
         #endregion
     }
 }
