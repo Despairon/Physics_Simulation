6d1655015628eb4c7d9b1beb68a3effabdc11f3a
Despairon <MaxDespairon@gmail.com>
2017-07-23 22:39:23 +0300

    completed VBO+VAO implementation. Need some minor refactor

--- Physics_Simulation/RenderObject.cs
+++ Physics_Simulation/RenderObject.cs
@@ -60,26 +60,29 @@ namespace Physics_Simulation
     {
         #region private_members
 
-        private List<Vector3>  _vertices;
-        private Color          _color;
-        private Transform      _transform;
-        private string         _name;
-        // private List<Triangle> _indices; // TODO: will be needed in shaders
+        private List<Vector3>   _vertices;
+        private List<Vector2>   _texcoords;
+        private List<Vector3>   _normals;
+        private VAO             _vao;
+        private Color           _color;
+        private Transform       _transform;
+        private string          _name;
+        private Primitives_type _type;
 
         #endregion
 
         #region public_members
 
-        public List<Vector3> vertices
+        public enum Primitives_type
         {
-            get
-            {
-                return _vertices;
-            }
-            private set
-            {
-                _vertices = value;
-            }
+            POINTS         = Gl.GL_POINTS,
+            LINE_STRIP     = Gl.GL_LINE_STRIP,
+            LINE_LOOP      = Gl.GL_LINE_LOOP,
+            LINES          = Gl.GL_LINES,
+            TRIANGLE_STRIP = Gl.GL_TRIANGLE_STRIP,
+            TRIANGLE_FAN   = Gl.GL_TRIANGLE_FAN,
+            TRIANGLES      = Gl.GL_TRIANGLES,
+            POLYGON        = Gl.GL_POLYGON
         }
 
         public Color color
@@ -94,37 +97,95 @@ namespace Physics_Simulation
             private set { _name = value; }
         }
 
-        public RenderObject(string name, List<Vector3> vertices, Color color)
+        public Primitives_type type
         {
-            this.vertices = new List<Vector3>();
-
-            if (vertices != null)
-                this.vertices = vertices;
-
-            this.color = color;
-
-            _transform = new Transform(0);
-
-            this.name = name;
+            get         { return _type; }
+            private set { _type = value; }
         }
 
-        public RenderObject(string name, List<Triangle> triangles, Color color)
+        public RenderObject(string name, List<Vector3> vertices, List<Vector2> texcoords, List<Vector3> normals, Color color, Primitives_type type)
         {
-            vertices = new List<Vector3>();
+            this.name = name;
 
-            if (triangles != null)
-                foreach (var triangle in triangles)
-                {
-                    vertices.Add(triangle.a);
-                    vertices.Add(triangle.b);
-                    vertices.Add(triangle.c);
-                }
+            _vertices  = vertices;
+            _texcoords = texcoords;
+            _normals   = normals;
 
             this.color = color;
 
+            this.type = type;
+
             _transform = new Transform(0);
 
-            this.name = name;
+            _vao = new VAO();
+            _vao.bind();
+
+            // TODO: try to refactor this implemetation. Seems to be bad.
+            int vbo_index = 0;
+
+            if (_vertices != null)
+            {
+                float[] vertices_data = new float[_vertices.Count * 3];
+
+                int i = 0;
+                foreach (var vertex in _vertices)
+                {
+                    vertices_data[i]     = (float)vertex.x;
+                    vertices_data[i + 1] = (float)vertex.y;
+                    vertices_data[i + 2] = (float)vertex.z;
+
+                    i += 3;
+                }
+
+                VBO vertex_VBO = new VBO(vbo_index, VBO.VBO_Type.ARRAY_BUFFER, vertices_data, VBO.VBO_Elements_per_vertex.THREE_ELEMENTS);
+
+                _vao.add_VBO(vertex_VBO);
+
+                vbo_index++;
+            }
+
+            if (_texcoords != null)
+            {
+                float[] texcoord_data = new float[_texcoords.Count * 2];
+
+                int i = 0;
+                foreach (var texcoord in _texcoords)
+                {
+                    texcoord_data[i]     = (float)texcoord.x;
+                    texcoord_data[i + 1] = (float)texcoord.y;
+
+                    i += 2;
+                }
+
+                VBO texcoords_VBO = new VBO(vbo_index, VBO.VBO_Type.ARRAY_BUFFER, texcoord_data, VBO.VBO_Elements_per_vertex.TWO_ELEMENTS);
+
+                _vao.add_VBO(texcoords_VBO);
+
+                vbo_index++;
+            }
+
+            if (_normals != null)
+            {
+                float[] normal_data = new float[_normals.Count * 3];
+
+                int i = 0;
+                foreach (var normal in _normals)
+                {
+                    normal_data[i]     = (float)normal.x;
+                    normal_data[i + 1] = (float)normal.y;
+                    normal_data[i + 2] = (float)normal.z;
+
+                    i += 3;
+                }
+
+                VBO normals_VBO = new VBO(vbo_index, VBO.VBO_Type.ARRAY_BUFFER, normal_data, VBO.VBO_Elements_per_vertex.THREE_ELEMENTS);
+
+                _vao.add_VBO(normals_VBO);
+
+                vbo_index++;
+            }
+
+            _vao.unbind();
         }
 
         public void draw()
@@ -143,12 +204,13 @@ namespace Physics_Simulation
 
             Gl.glScaled(_transform.scale.x, _transform.scale.y, _transform.scale.z );
 
-            Gl.glBegin(Gl.GL_TRIANGLES);
-    
-            foreach (var vertex in vertices)
-                Gl.glVertex3f((float)vertex.x, (float)vertex.y, (float)vertex.z);
+            _vao.bind();
+                _vao.enable(0);
 
-            Gl.glEnd();
+                    Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, _vertices.Count);
+
+                _vao.disable(0);
+            _vao.unbind();
 
             Gl.glPopMatrix();
         }
@@ -179,22 +241,41 @@ namespace Physics_Simulation
                 return null;
             else
             {
-                // TODO: this is very bad implementation. Rework it to make with VBO
-
                 string name = objFile.name;
 
-                List<Triangle> triangles = new List<Triangle>();
+                List<Vector3> vertices  = new List<Vector3>();
+                List<Vector2> texcoords = new List<Vector2>();
+                List<Vector3> normals   = new List<Vector3>();
+
+                Primitives_type type = Primitives_type.TRIANGLES;
+
+                if (objFile.meshes.Exists(mesh => (mesh.faces.Exists(face => face.vertex_indices.Length == 3))))
+                    type = Primitives_type.TRIANGLES;
+                else if (objFile.meshes.Exists(mesh => (mesh.faces.Exists(face => face.vertex_indices.Length > 3))))
+                    type = Primitives_type.POLYGON;
+                else
+                    type = Primitives_type.POINTS;
 
                 foreach (var mesh in objFile.meshes)
                     foreach (var face in mesh.faces)
                     {
-                        Vector3 _a = objFile.vertices[face.vertex_indices[0] - 1];
-                        Vector3 _b = objFile.vertices[face.vertex_indices[1] - 1];
-                        Vector3 _c = objFile.vertices[face.vertex_indices[2] - 1];
+                        foreach (var vertex_index in face.vertex_indices)
+                        {
+                            var vertex = objFile.vertices[vertex_index - 1];
+                            vertices.Add(vertex);
+                        }
 
-                        Triangle triangle = new Triangle(_a, _b, _c);
+                        foreach (var texcoord_index in face.texcoord_indices)
+                        {
+                            var texcoord = objFile.texcoords[texcoord_index - 1];
+                            texcoords.Add(texcoord);
+                        }
 
-                        triangles.Add(triangle);
+                        foreach (var normal_index in face.normal_indices)
+                        {
+                            var normal = objFile.normals[normal_index - 1];
+                            normals.Add(normal);
+                        }
                     }
 
                 Random rnd = new Random(DateTime.Now.Millisecond);
@@ -204,7 +285,7 @@ namespace Physics_Simulation
 
                 Color c = Color.FromArgb(r, g, b);
 
-                var obj = new RenderObject(name, triangles, c);
+                var obj = new RenderObject(name, vertices, texcoords, normals, c, type);
 
                 return obj;
             }
--- Physics_Simulation/VBO.cs
+++ Physics_Simulation/VBO.cs
@@ -11,24 +11,43 @@ namespace Physics_Simulation
     {
         #region private_members
 
-        private int     _id;
-        private float[] _data;
+        private int      _id;
+        private VBO_Type _type;
+        private float[]  _data;
 
         #endregion
 
         #region public_members
 
-        public VBO(int attribute_index, int type, float[] data, int dimensions)
+        public enum VBO_Elements_per_vertex
+        {
+            NULL = 0,
+            ONE_ELEMENT,
+            TWO_ELEMENTS,
+            THREE_ELEMENTS
+        }
+        
+        public enum VBO_Type
+        {
+            ARRAY_BUFFER         = Gl.GL_ARRAY_BUFFER,
+            ELEMENT_ARRAY_BUFFER = Gl.GL_ELEMENT_ARRAY_BUFFER,
+            PIXEL_PACK_BUFFER    = Gl.GL_PIXEL_PACK_BUFFER,
+            PIXEL_UNPACK_BUFFER  = Gl.GL_PIXEL_UNPACK_BUFFER
+        }
+
+        public VBO(int attribute_index, VBO_Type type, float[] data, VBO_Elements_per_vertex elements_per_vertex)
         {
             this.data = data;
+
+            this.type = type;
 
             Gl.glGenBuffers(1, out _id);
 
             bind();
 
-            Gl.glBufferData(type, (IntPtr)(data.Length), data, Gl.GL_STATIC_DRAW);
+            Gl.glBufferData((int)type, (IntPtr)(data.Length * sizeof(float)), data, Gl.GL_STATIC_DRAW);
 
-            Gl.glVertexAttribPointer(attribute_index, dimensions, Gl.GL_FLOAT, Gl.GL_FALSE, 0, 0);
+            Gl.glVertexAttribPointer(attribute_index, (int)elements_per_vertex, Gl.GL_FLOAT, Gl.GL_FALSE, 0, IntPtr.Zero);
 
             unbind();
         }
@@ -45,14 +64,20 @@ namespace Physics_Simulation
             private set { _data = value;}
         }
 
+        public VBO_Type type
+        {
+            get         { return _type; }
+            private set { _type = value; }
+        }
+
         public void bind()
         {
-            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, id);
+            Gl.glBindBuffer((int)type, id);
         }
 
         public void unbind()
         {
-            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, 0);
+            Gl.glBindBuffer((int)type, 0);
         }
 
         public void delete()
